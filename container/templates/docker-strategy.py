ffc224ebd72b501549055a625090ddfe2ba87d14
Joshua "jag" Ginsberg <jag@flowtheory.net>
2017-01-13 14:10:20 -0500

    Fingerprint calculation fixes

--- container/templates/docker-strategy.py
+++ container/templates/docker-strategy.py
@@ -30,12 +30,13 @@ NOOP_TASKS = [
 
 def debug(msg):
     if os.environ.get('ANSIBLE_CONTAINER_DEBUG'):
-        display.display(msg)
+        display.display('ac-strategy-debug: %s' % msg)
 
 class StrategyModule(LinearStrategyModule):
 
     last_role_per_host = {}
     last_block_per_host = {}
+    fingerprint_hash = {}
     _client = None
     project_name = 'ansible'
 
@@ -100,7 +101,7 @@ class StrategyModule(LinearStrategyModule):
         """
         client = self.get_client()
         container_config = client.inspect_container(self.container_id_for_host(host))['Config']
-        debug('%s' % container_config)
+        # debug('%s' % container_config)
         name = self.container_name_for_host(host)
         client.remove_container(name)
         container_config['Image'] = image_id
@@ -120,44 +121,8 @@ class StrategyModule(LinearStrategyModule):
         parent_results = super(StrategyModule, self)._get_next_task_lockstep(hosts, iterator)
         parent_results_dict = dict(parent_results)
         for host in hosts:
-            try:
-                display.display('Getting next task for %s' % host)
-                task = parent_results_dict.get(host, None)
-                debug('Host = %s, task = %s' % (host, task))
-                role = getattr(task, '_role', None)
-                # This changed between 2.1 and 2.2, so support both
-                block = getattr(task, '_parent',
-                                getattr(task, '_block', None) or getattr(task, '_task_include', None))
-                debug('Current task role: %s, current task block: %s' % (
-                    role, block))
-                # So commit after every role, every play that has tasks that
-                # are not in roles, and every include
-                role_changed = (self.last_role_per_host.get(host, None) and
-                               self.last_role_per_host[host] != role)
-                # The linear strategy spits out a None task at the end of a play
-                is_new_block = task is None or (not role and role_changed
-                                                and repr(task) not in NOOP_TASKS)
-                is_new_role = role and role_changed
-                debug('is_new_block: %s, is_new_role: %s' % (is_new_block, is_new_role))
-                if is_new_block or is_new_role:
-                    hash_obj = hashlib.sha256()
-                    if is_new_role:
-                        self.hash_role(hash_obj, self.last_role_per_host[host])
-                    else:
-                        self.hash_block(hash_obj, self.last_block_per_host[host])
-                    image_id = self.commit_layer(host, hash_obj.hexdigest())
-                    self.switch_to_image(host, image_id)
-                self.last_block_per_host[host] = block
-                self.last_role_per_host[host] = role
-            except Exception, e:
-                import epdb
-                epdb.serve()
-                raise
-
-        """
-            if repr(task) in NOOP_TASKS:
-                debug('Non-commitable task: %s' % task)
-                continue
+            display.display('Getting next task for %s' % host)
+            task = parent_results_dict.get(host, None)
             debug('Host = %s, task = %s' % (host, task))
             role = getattr(task, '_role', None)
             # This changed between 2.1 and 2.2, so support both
@@ -165,38 +130,48 @@ class StrategyModule(LinearStrategyModule):
                             getattr(task, '_block', None) or getattr(task, '_task_include', None))
             debug('Current task role: %s, current task block: %s' % (
                 role, block))
-            # import epdb; epdb.serve()
-            # If we've changed to a new play, a new include, or a new role,
-            # commit a layer
-            last_role = self.last_role_per_host.get(host, None)
-            last_block = self.last_block_per_host.get(host, None)
-            debug('Last role for this host: %s, last block for this host: %s' % (
-                last_role, last_block))
-            if last_block is not block:
-                if last_block:
-                    hash_obj = hashlib.sha256()
-                    self.hash_block(hash_obj, last_block)
-                    image_id = self.commit_layer(host,
-                                                 hash_obj.hexdigest())
-                    self.switch_to_image(host, image_id)
+            if os.environ.get('ANSIBLE_CONTAINER_DEBUG') and repr(task) not in NOOP_TASKS:
+                role_hash = self.fingerprint_hash.get(host, hashlib.sha256()).copy()
+                block_hash = self.fingerprint_hash.get(host, hashlib.sha256()).copy()
+                if role:
+                    self.hash_role(role_hash, role)
+                    debug('Role hash: %s' % role_hash.hexdigest())
+                if block:
+                    self.hash_block(block_hash, block)
+                    debug('Block hash: %s' % block_hash.hexdigest())
+            # So commit after every role, every play that has tasks that
+            # are not in roles, and every include
+            role_changed = (self.last_role_per_host.get(host, None) and
+                           self.last_role_per_host[host] != role)
+            # The linear strategy spits out a None task at the end of a play
+            is_new_block = task is None or (not role and role_changed
+                                            and repr(task) not in NOOP_TASKS)
+            is_new_role = role and role_changed
+            debug('is_new_block: %s, is_new_role: %s' % (is_new_block, is_new_role))
+            if is_new_block or is_new_role:
+                # Initialize the fingerprint hash with the current layer's
+                # hash
+                hash_obj = self.fingerprint_hash.get(host, hashlib.sha256())
+                if is_new_role:
+                    self.hash_role(hash_obj, self.last_role_per_host[host])
+                else:
+                    self.hash_block(hash_obj, self.last_block_per_host[host])
+                image_id = self.commit_layer(host, hash_obj.hexdigest())
+                self.switch_to_image(host, image_id)
+                self.fingerprint_hash[host] = hash_obj
+            # flush_handlers appears as a different block
+            if repr(task) not in NOOP_TASKS:
                 self.last_block_per_host[host] = block
-            elif last_role is not role:
-                if last_role:
-                    hash_obj = hashlib.sha256()
-                    self.hash_block(hash_obj, last_role)
-                    image_id = self.commit_layer(host,
-                                                 hash_obj.hexdigest())
-                    self.switch_to_image(host, image_id)
-                self.last_role_per_host[host] = role
-        """
+            self.last_role_per_host[host] = role
+
         return parent_results
 
     def hash_task(self, hash_obj, task):
-        debug(u'Task attributes: %s' % task._attributes)
+        # debug(u'Task attributes: %s' % task._attributes)
         hash_obj.update(repr(task._attributes))
 
     def hash_block(self, hash_obj, play):
-        debug(u'Play attributes: %s' % play._attributes)
+        # debug(u'Play attributes: %s' % play._attributes)
         hash_obj.update(repr(play._attributes))
         for task in play.block:
             self.hash_task(hash_obj, task)
@@ -211,11 +186,11 @@ class StrategyModule(LinearStrategyModule):
                 hash_obj.update(data)
 
     def hash_dir(self, hash_obj, dir_path):
-        debug('hash_dir: dir_path = %s' % (dir_path,))
+        # debug('hash_dir: dir_path = %s' % (dir_path,))
         for root, dirs, files in os.walk(dir_path, topdown=True):
             for file_path in files:
                 abs_file_path = os.path.join(root, file_path)
-                debug('hash_dir: abs_file_path = %s' % (abs_file_path,))
+                # debug('hash_dir: abs_file_path = %s' % (abs_file_path,))
                 hash_obj.update(abs_file_path)
                 self.hash_file(hash_obj, abs_file_path)
 
@@ -223,7 +198,7 @@ class StrategyModule(LinearStrategyModule):
         # A role is easy to hash - the hash of the role content with the
         # hash of any role dependencies it has
         role_path = role._role_path
-        debug('hash_role: role_path = %s' % (role_path,))
+        # debug('hash_role: role_path = %s' % (role_path,))
         self.hash_dir(hash_obj, role_path)
         for dependency in role._dependencies:
             self.hash_role(hash_obj, dependency)
